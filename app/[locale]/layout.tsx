caf091018158bc813022cbe0484c436d2617b189
nick20name17 <nick20name17@gmail.com>
2025-11-03 18:43:19 +0200

    add seo tags

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,9 @@
 import { routing } from '@/i18n/routing'
 import { Metadata } from 'next'
-import { getTranslations } from 'next-intl/server'
+import { getTranslations, setRequestLocale } from 'next-intl/server'
 import { Comfortaa } from 'next/font/google'
 import { notFound } from 'next/navigation'
+import Script from 'next/script'
 import { ScrollToTop } from './elements/scroll-to-top/ScrollToTop'
 import './globals.css'
 import { Footer } from './sections/footer/Footer'
@@ -12,6 +13,10 @@ const comfortaa = Comfortaa({
     variable: '--comfortaa',
     subsets: ['cyrillic', 'cyrillic-ext', 'latin', 'latin-ext'],
 })
+
+export function generateStaticParams() {
+    return routing.locales.map((locale) => ({ locale }))
+}
 
 export async function generateMetadata({
     params: { locale },
@@ -37,6 +42,8 @@ export default async function LocaleLayout({
         notFound()
     }
 
+    setRequestLocale(locale)
+
     const t = await getTranslations('Nav')
 
     return (
@@ -53,8 +60,63 @@ export default async function LocaleLayout({
                     href="/apple-touch-icon.png"
                     color="#ffffff"
                 />
+                <Script
+                    id="gtm-head"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WP4MT8NM');`,
+                    }}
+                />
+
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-17648212179"
+                    strategy="afterInteractive"
+                />
+
+                <Script
+                    id="gtag-init"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17648212179');
+            `,
+                    }}
+                />
+                <Script
+                    id="meta-pixel"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '682862281271145');
+              fbq('track', 'PageView');
+            `,
+                    }}
+                />
             </head>
             <body className={`${comfortaa.className}`}>
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WP4MT8NM"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 <Nav
                     description={t('description')}
                     about={t('about')}
