ba38611931c77562c6d2bdec71fa68bb5725fd00
nick20name17 <nick20name17@gmail.com>
2025-11-10 21:38:10 +0200

    add seo tags

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -90,21 +90,63 @@ export default async function LocaleLayout({
                     }}
                 />
                 <Script
-                    id="meta-pixel"
+                    id="facebook-pixel"
                     strategy="afterInteractive"
                     dangerouslySetInnerHTML={{
                         __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){
+                n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)
+              };
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
               t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '682862281271145');
-              fbq('track', 'PageView');
-            `,
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1402415434776405');
+            fbq('track', 'PageView');
+          `,
+                    }}
+                />
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=1402415434776405&ev=PageView&noscript=1"
+                        alt=""
+                    />
+                </noscript>
+
+                <Script
+                    id="google-ads-conversion"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+          gtag('event', 'conversion', {
+            'send_to': 'AW-17711540948/jfe5CJOCtL0bENTVwv1B',
+            'value': 1.0,
+            'currency': 'UAH'
+          });
+        `,
+                    }}
+                />
+
+                <Script
+                    id="google-tag-manager"
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-17711540948"
+                />
+                <Script
+                    id="google-tag-manager-config"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17711540948');
+          `,
                     }}
                 />
             </head>
