bb5ba40c53270342c7f7c508ec9bb565d60e3870
nick20name17 <nick20name17@gmail.com>
2025-11-24 23:05:25 +0200

    fix link for nav

--- src/components/layout/header/header-nav.tsx
+++ src/components/layout/header/header-nav.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
-import Link from 'next/link'
 
 import { LangSelect } from './lang-select'
+import { Link } from '@/i18n/navigation'
 import { cn } from '@/lib/utils'
 
 interface HeaderNavProps {
