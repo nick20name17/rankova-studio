a48a90afa2b65939d63b4e640bffafdffe8bde27
nick20name17 <nick20name17@gmail.com>
2025-11-27 13:48:10 +0200

    fixes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   token: process.env.SANITY_TOKEN,
-  useCdn: false // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
