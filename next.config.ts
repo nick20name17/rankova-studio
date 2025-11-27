5c163e526ac9c383a3bb81d76d4780de4ef6106c
nick20name17 <nick20name17@gmail.com>
2025-11-27 20:34:28 +0200

    fixes

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,10 @@ import { type NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
-  // reactCompiler: true,
+  reactCompiler: true,
   experimental: {
     inlineCss: true
   },
-  turbopack: {
-    resolveAlias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      util: 'util',
-      buffer: 'buffer'
-    }
-  },
-  transpilePackages: ['next-intl', '@sanity/image-url', 'sonner', 'embla-carousel-react'],
   images: {
     minimumCacheTTL: 60 * 60 * 24 * 7,
     qualities: [75, 100],
