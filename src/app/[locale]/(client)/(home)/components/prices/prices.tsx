dd47b8e64a82290a5a641451324c481b828da190
nick20name17 <nick20name17@gmail.com>
2025-11-28 17:14:03 +0200

    add testings logs

--- src/app/[locale]/(client)/(home)/components/prices/prices.tsx
+++ src/app/[locale]/(client)/(home)/components/prices/prices.tsx
@@ -22,6 +22,8 @@ export const PricesSection = async () => {
 
   const prices = (await client.fetch(PRICES_QUERY, {}, options)) as PriceSection[]
 
+  console.log(prices)
+
   return (
     <Section
       className='overflow-x-hidden'
