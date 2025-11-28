69212de248457856961337f527590a0ba85182d7
nick20name17 <nick20name17@gmail.com>
2025-11-28 18:26:40 +0200

    fixes

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,8 +3,8 @@ import { defineRouting } from 'next-intl/routing'
 export const routing = defineRouting({
   locales: ['en', 'uk'],
   defaultLocale: 'uk',
-  localeCookie: false,
-  localePrefix: 'as-needed'
+  localeCookie: false
+  // localePrefix: 'as-needed'
 })
 
 export type Locale = (typeof routing.locales)[number]
